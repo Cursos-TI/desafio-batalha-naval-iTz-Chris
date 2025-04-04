01dcc91e369ddd013e85853c387bfed60aec4a9a
Christian Costa <136290447+iTz-Chris@users.noreply.github.com>
2025-04-04 15:11:08 -0300

    Update batalhaNaval.c - Nível Aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -3,7 +3,7 @@
 int main() {
 
     // Matriz tabuleiro 10x10
-    int batalhaNaval[10][10] = {0};
+    int tabuleiro[10][10] = {0};
 
 
     // Navio 1 (horizontal)
@@ -18,24 +18,47 @@ int main() {
     int navio2_coluna = 6;
 
 
+    // Navio 3 (diagonal)
+    int navio3[3] = {3, 3, 3};
+    int navio3_linha = 2;
+    int navio3_coluna = 2;
+
+
+    // Navio 4 (diagonal)
+    int navio4[3] = {3, 3, 3};
+    int navio4_linha = 7;
+    int navio4_coluna = 4;
+
+
     // Posicionando os navios no tabuleiro:
 
+    // Navio 1
     for (int index = 0; index < 3; index++) {
-        batalhaNaval[navio1_coluna][navio1_linha + index] = navio1[index];
+        tabuleiro[navio1_coluna][navio1_linha + index] = navio1[index];
     }
 
+    // Navio 2
     for (int index = 0; index < 3; index++) {
-        batalhaNaval[navio2_coluna + index][navio2_linha] = navio2[index];
+        tabuleiro[navio2_coluna + index][navio2_linha] = navio2[index];
+    }
+
+    // Navio 3
+    for (int index = 0; index < 3; index++) {
+        tabuleiro[navio3_linha + index][navio3_coluna + index] = navio3[index];
+    }
+
+    // Navio 4
+    for (int index = 0; index < 3; index++) {
+        tabuleiro[navio4_linha - index][navio4_coluna + index] = navio4[index];
     }
 
 
     // Exibindo o tabuleiro:
-    printf ("Tabuleiro de Batalha Naval: \n");
+    printf("Tabuleiro de Batalha Naval: \n");
 
     for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            
-            printf("%d ", batalhaNaval[i][j]);
+        for (int j = 0; j < 10; j++) {     
+            printf("%2d ", tabuleiro[i][j]);
         }
         printf("\n");
     }
